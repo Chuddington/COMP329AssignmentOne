f247c05ddee4fed0b5fa3c1ce0603e09bf509ff1
Chuddington <michaelchadwick94@gmail.com>
2015-11-03 10:07:08 +0000

    I think I've finished the Bluetooth Connection methods

--- Java/BluetoothStuff.java
+++ Java/BluetoothStuff.java
@@ -1,30 +1,37 @@
 import java.io.*;
 import lejos.nxt.*;
 import lejos.nxt.comm.*;
-import lejos.pc.tools;
 
 public class BluetoothStuff {
 
+  //global variables
   static int btPin = 4319;
+  
+  //Method to start a Bluetooth Connection to nxjconsoleviewer.bat
+  //nxjconsoleviewer should be open and ready to connect when this method is
+  //called.
+  public static void startBtConn() {
+    LCD.drawString("Waiting for \nRConsole \nConnection", 0, 1);
+    RConsole.openBluetooth(0); //wait indefinitely for a BT connection
+    //test output: sends the BT PIN to the RConsole
+    RConsole.println("RConsole Connected; PIN: " + Integer.toString(btPin) );
+  }
 
-  public static void main(String[] args) {
+  //method to close the RConsole connection to the Computer
+  public static void closeBtConn() {
+    LCD.drawString("Closing BT Connection", 0, 1);
+    RConsole.println("Closing BT Connection");
+    RConsole.close();
+  }
 
-    LCD.drawString("right BT",0, 0);
-    NXTConnection connection = null;
+  //method to print a String input to the RConsole
+  public static void stringToRCon(String s) {
+    RConsole.println(s);
+  }
 
-      LCD.drawString("waiting for BT", 0,1 );
-      RConsole.open();
-      RConsole.openBluetooth(10000);
-      connection = Bluetooth.waitForConnection();
-
-    DataOutputStream dataOut = connection.openDataOutputStream();
-
-    try {
-      dataOut.writeInt(btPin);
-    } catch (IOException e ) {
-      System.out.println(" write error "+e); 
-    }
-
+  //method to print a Pose input to the RConsole
+  public static void poseToRCon(Pose p) {
+    RConsole.println(Pose.toString(p) );
   }
 
 }
