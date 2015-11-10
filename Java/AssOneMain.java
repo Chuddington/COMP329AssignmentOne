d856ea9422edec9e682b8ed6983d2296002e47c6
Michael Chadwick <michaelchadwick94@gmail.com>
2015-11-10 23:48:12 +0000

    Formatted Main Class
    Now it looks nice and stuff

--- Java/AssOneMain.java
+++ Java/AssOneMain.java
@@ -20,128 +20,133 @@ import lejos.nxt.Motor;
  
 public class AssOneMain {
  
-    //global variables here - templates for now
-    static int columns, numOfRowCells = 0;   
-    static MapSystem mapObj                  ;
-    static BtStuff   btObj  = new BtStuff()  ;
-    static Movement  movObj;
+  //global variables here - templates for now
+  static int       columns       = 0            ; //grid X axis
+  static int       numOfRowCells = 0            ; //grid Y axis
+  static BtStuff   btObj         = new BtStuff(); //var to connect to RConsole
+  static boolean   objAhead      = false        ; //true if front cell is full
+  static boolean   objLeft       = false        ; //true if left  cell is full
+  static boolean   objRight      = false        ; //true if right cell is full
+  static Movement  movObj; //given value in main
+  static MapSystem mapObj; //given value in main
 
-    static boolean objAhead = false;
-    static boolean objLeft  = false;
-    static boolean objRight = false;
+  public static void main(String[] args) {
+    //grid values for mapping
+    columns       = 5;
+    numOfRowCells = 7;
+
+    //connect to other classes
+    mapObj = new MapSystem(columns, numOfRowCells);
+    movObj = new Movement( columns, numOfRowCells);
     
-
-    public static void main(String[] args) {
-        //grid values for mapping
-        columns = 5;
-        numOfRowCells = 7;
-        
-        mapObj = new MapSystem(columns, numOfRowCells);
-        movObj = new Movement(columns, numOfRowCells); 
-        btObj.startBtConn();
+    //connect to RConsole
+    btObj.startBtConn();
             
-        //for each cell in a Row
-        for(int loop1 = 0; loop1 < columns; loop1++) {
-            for(int loop2 = 0; loop2 < numOfRowCells; loop2++) {
-                movRow();
-            }
-            if(loop1 % 2 == 0) {
-                movCol(true);
-            } else {
-                movCol(false);
-            }
-        }
+    //for each column
+    for(int loop1 = 0; loop1 < columns; loop1++) {
+      //for each cell within the column
+      for(int loop2 = 0; loop2 < numOfRowCells; loop2++) {
+        movRow(); //scan and move method
+      }
+      if(loop1 % 2 == 0) { //if the robot moves along the Y axis positively
+        movCol(true) ;     //turn right into the new column
+      } else {
+        movCol(false);     //turn left  into the new column
+      }
     }
+  }
+  
+  //Method scans input and maps it; if obstacle is in front, left and right,
+  //backpedal.  If there is a space adjacent but an obstacle at the front,
+  //turns to move around the obstacle using the available space.
+  public static void movRow() {
+    double nextCell = mapObj.basicProb(); //work out probability
+    //output to RConsole through BlueTooth
+    btObj.stringToRCon("Object Probability in next Cell: %.2f" + nextCell);
+
+    //sonar scan around of the robot
+    objLeft  = mapObj.scanLeft() ;
+    objAhead = mapObj.scanAhead();
+    objRight = mapObj.scanRight();
+        
+    //mapObj.printMap(columns, numOfRowCells);
+        
+    if(objAhead) { //if there's an obstacle in front
+      //if there's an obstacle to the left & right.  i.e. a dead end
+      if(objLeft && objRight) {
+        turnAround(objLeft, mapObj, movObj); //move backwards
+      } else {
+        //call movAround() method to navigate around the obstacle
+        movAround( objLeft, mapObj, movObj);
+      }
+    } else {
+      movObj.nextCell(mapObj); //move forward a cell
+    }
+  }
+  
+  //Method to shift columns (X Axis)
+  public static void movCol(boolean b) {
+    movObj.turn(b, mapObj); //rotate 90 degrees - true = right; false = left
+    movRow()              ; //move forward a cell
+    movObj.turn(b, mapObj); //rotate 90 degrees - true = right; false = left
+      
+  }
     
-    public static void movRow() {
-        //work out probability
-        double nextCell = mapObj.basicProb();
-        btObj.stringToRCon("Object Probability in next Cell: " + nextCell);
-        //sonar scan in front of the robot
-        objLeft  = mapObj.scanLeft() ;
-        objAhead = mapObj.scanAhead();
-        objRight = mapObj.scanRight();
-        
-       // mapObj.printMap(columns, numOfRowCells);
-        
-        //if obstacle detected:
-        if(objAhead) {
-            //if there's an obstacle to the left & right.  i.e. a dead end
-            if(objLeft && objRight) {
-                turnAround(objLeft, mapObj, movObj);
-            } else {
-                //call movAround() method to navigate around the obstacle
-                movAround(objLeft, mapObj, movObj);
-            }
-        } else {
-            //move forward a cell
-            movObj.nextCell(mapObj);
-        }
-    }
+  //Method to move around an obstacle - runs when obstacle is in front
+  public static void movAround(boolean r, MapSystem ms, Movement mv) {
+    mv.turn(r, ms) ; //true = right turn; false = left turn
+    movRow()       ; //scan and move forward if necessary
+    mv.turn(!r, ms); //invert the boolean to turn the other way
     
-    public static void movCol(boolean b) {
-        //rotate 90 degrees to the right
-        movObj.turn(b, mapObj);
-        //move forward a cell
-        movRow();
-        //rotate 90 degrees to the right
-        movObj.turn(b, mapObj);
-        
-    }
+    //move forward twice to pass the obstacle
+    movRow();
+    movRow();
     
-    //Method to move around a single obstacle - runs when obstacle is detected ahead but not in a dead end
-    public static void movAround(boolean r, MapSystem ms, Movement mv) {
-        //turn to the right if true (if an obstacle is on the left, for example)
-        mv.turn(r, ms);
-        //scan and move forward if necessary
-        movRow();
-        //invert the boolean to turn the other way
-        mv.turn(!r, ms);
-        //move forward twice to pass the obstacle
-        movRow();
-        movRow();
-        //turn and move to the correct column the robot should be in
-        mv.turn(!r, ms);
-        boolean extend = mapObj.scanAhead();
-        while(extend) {
-            mv.turn(r, ms);
-            movRow();
-            mv.turn(!r, ms);
-        }
-        movRow();
-        //face the correct way to continue the patrol
-        mv.turn(r, ms);   
+    mv.turn(!r, ms);                     //turn to face original column
+    boolean extend = mapObj.scanAhead(); //flag to check object is passed
+    while(extend) {
+      mv.turn( r, ms) ;          //turn to move along the axis
+      movObj.nextCell(mapObj);
+      mv.turn(!r, ms) ;          //face the original column
+      if(!mapObj.scanAhead() ) { //if an obstacle is not found anymore
+        extend = false;          //break the while loop
+      } else {
+      }
     }
+    movRow();       //move into the original column
+    mv.turn(r, ms); //face the correct way to continue the patrol
+  }
     
-    //method to run when the robot is in a dead end and has to back up and go around
-    public static void turnAround(boolean r, MapSystem ms, Movement mv) {
-        //turn around and move back a square
-        mv.turn(ms);
-        movRow();
-        //check for empty adjacent space
-        objLeft  = mapObj.scanLeft() ;
-        objRight = mapObj.scanRight();
-        //Move to said empty space
-        if(objLeft && !objRight) { //Obstacle on the Left
-            mv.turn(objLeft, ms);
-            movRow();
-        } else if(objRight && !objLeft) { //Obstacle on the Right
-            mv.turn(objRight, ms);
-            movRow();
-        } else { //in a corridor
-            movRow();
-        }
-        
-        //move forward 2 cells, scanning as we go
-        //if an obstacle is still on the same axis as the first
-        //    class it as a different obstacle
-        //    move forward a cell
-        //    scan again - recursive
-        //move back to the correct column
-        
-        //if at a 'limit cell', turn left instead of right
-        
+  //method runs when robot is in a dead end and has to back up and go around
+  public static void turnAround(boolean r, MapSystem ms, Movement mv) {
+    mv.turn(ms); //180 degree turn
+    movRow()   ; //move forward
+
+    //check for empty adjacent space
+    objLeft  = mapObj.scanLeft() ;
+    objRight = mapObj.scanRight();
+
+    //Move to said empty space
+    if(objLeft && !objRight) { //Obstacle on the Left
+      mv.turn(objLeft,  ms);   //turn right
+      movRow();
+    } else if(objRight && !objLeft) { //Obstacle on the Right
+      mv.turn(!objRight, ms);  //turn left
+      movRow();
+    } else { //in a corridor
+      movRow();
     }
+        
+    //move forward 2 cells, scanning as we go
+    //if an obstacle is still on the same axis as the first
+    //    class it as a different obstacle
+    //    move forward a cell
+    //    scan again - recursive
+    //move back to the correct column
+        
+    //if at a 'limit cell', turn left instead of right
+        
+  }
     
-    }
+}
 //EndOfFile
